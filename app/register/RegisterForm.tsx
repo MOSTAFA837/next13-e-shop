aa52c42dd53df7c8be195a85d497b850f868f110
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-10-23 15:30:18 +0200

    register user

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,6 +7,10 @@ import { Input } from "../components/inputs/Input";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { Button } from "../components/Button";
 import Link from "next/link";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function RegisterForm() {
   const [isLoading, setIsLoading] = useState(false);
@@ -22,10 +26,34 @@ export default function RegisterForm() {
     },
   });
 
+  const router = useRouter();
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
-    console.log(data);
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        toast.success("Account created successfully");
+
+        signIn("credentials", {
+          email: data.email,
+          password: data.password,
+          redirect: false,
+        }).then((callback) => {
+          if (callback?.ok) {
+            router.push("/cart");
+            router.refresh();
+            toast.success("Logged in");
+          }
+
+          if (callback?.error) {
+            toast.error(callback.error);
+          }
+        });
+      })
+      .catch(() => toast.error("Somethig went wrong"))
+      .finally(() => setIsLoading(false));
   };
 
   return (
